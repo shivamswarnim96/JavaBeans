671a70a5b191eba36d0d8b7b7b53daa36a82d65e
shivamswarnim96 <33998787+shivamswarnim96@users.noreply.github.com>
2020-02-06 21:01:24 +0530

    Add files via upload

---
+++ ColorsBeanInfo.java
@@ -0,0 +1,19 @@
+package JavaBeans;
+
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+public class ColorsBeanInfo extends SimpleBeanInfo {
+	public PropertyDescriptor[] getPropertyDescriptors() {
+		try {
+			PropertyDescriptor rectangular = new
+			PropertyDescriptor("rectangular", Colors.class);
+			PropertyDescriptor pd[] = {rectangular};
+			return pd;
+		}
+		catch(Exception e) {
+			System.out.println("Exception caught. " + e);
+		}
+		return null;
+	}
+}
